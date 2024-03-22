222d2123a0b3ee84d50237bda49787c697c3cc37
areif-dev <ajreifsnyder@protonmail.com>
2024-03-22 01:14:06 -0400

    Set kernel to latest

--- configuration.nix
+++ configuration.nix
@@ -38,6 +38,8 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
