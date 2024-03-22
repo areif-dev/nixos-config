84a555e1f269216dc95ced6cff75b075a95a3476
areif-dev <ajreifsnyder@protonmail.com>
2024-03-22 01:08:40 -0400

    Add an overlay to pin waybar to 0.9.4

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    waybarpin.url = "github:nixos/nixpkgs/73bf415737ceb66a6298f806f600cfe4dccd0a41";
 
     # home-manager = {
     #   url = "github:nix-community/home-manager";
@@ -10,8 +11,13 @@
     # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, waybarpin, ... }@inputs:
     let
+      overlays = [
+        (final: prev: {
+          waybar = waybarpin.legacyPackages.${prev.system}.waybar;
+	})
+      ];
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
@@ -20,6 +26,9 @@
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
+	    ({...}: {
+	      nixpkgs.overlays = overlays;
+	    })
             ./configuration.nix
             # inputs.home-manager.nixosModules.default
           ];
