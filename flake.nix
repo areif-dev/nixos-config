8848b289b6eed7872334da3aadf31866bb5acb96
areif-dev <ajreifsnyder@protonmail.com>
2024-03-24 17:58:38 -0400

    Remove unnecessary waybar pin

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    waybarpin.url = "github:nixos/nixpkgs/73bf415737ceb66a6298f806f600cfe4dccd0a41";
 
     # home-manager = {
     #   url = "github:nix-community/home-manager";
@@ -11,13 +10,8 @@
     # };
   };
 
-  outputs = { self, nixpkgs, waybarpin, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
-      overlays = [
-        (final: prev: {
-          waybar = waybarpin.legacyPackages.${prev.system}.waybar;
-	})
-      ];
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
@@ -26,9 +20,6 @@
       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
           specialArgs = {inherit inputs;};
           modules = [ 
-	    ({...}: {
-	      nixpkgs.overlays = overlays;
-	    })
             ./configuration.nix
             # inputs.home-manager.nixosModules.default
           ];
