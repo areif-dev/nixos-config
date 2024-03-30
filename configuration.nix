5632d4949a5491f228eac9c1f67709d54f7a7ec0
areif-dev <ajreifsnyder@protonmail.com>
2024-03-29 23:49:52 -0400

    Change hostname to laptop

--- configuration.nix
+++ configuration.nix
@@ -40,7 +40,7 @@ in
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "laptop"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
