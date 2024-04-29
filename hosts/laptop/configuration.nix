d454f008c428aea46314ed8dde8026d99e15e00c
areif-dev <ajreifsnyder@protonmail.com>
2024-04-28 22:30:43 -0400

    Remove sway references

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -4,26 +4,6 @@
 
 { config, lib, pkgs, ... }:
 
-let
-  # bash script to let dbus know about important env variables and
-  # propagate them to relevent services run at the end of sway config
-  # see
-  # https://github.com/emersion/xdg-desktop-portal-wlr/wiki/"It-doesn't-work"-Troubleshooting-Checklist
-  # note: this is pretty much the same as  /etc/sway/config.d/nixos.conf but also restarts  
-  # some user services to make sure they have the correct environment variables
-  dbus-sway-environment = pkgs.writeTextFile {
-    name = "dbus-sway-environment";
-    destination = "/bin/dbus-sway-environment";
-    executable = true;
-
-    text = ''
-      dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
-      systemctl --user stop pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-      systemctl --user start pipewire pipewire-media-session xdg-desktop-portal xdg-desktop-portal-wlr
-    '';
-  }; 
-
-in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -158,11 +138,6 @@ in
   # };
 
   hardware.bluetooth.enable = true;
-
-  programs.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true;
-  };
 
   programs.river.enable = true; 
 
